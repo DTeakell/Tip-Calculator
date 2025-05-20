e1e5a0c29027d1c3f21c03f8e535234211c9604d
Dillon Teakell <teakelld@icloud.com>
2025-05-20 10:34:25 -0400

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Dillon Teakell on 5/20/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tip Calculator/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tip Calculator
+//
+//  Created by Dillon Teakell on 5/20/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
