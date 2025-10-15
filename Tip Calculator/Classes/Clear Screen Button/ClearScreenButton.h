f2372dc6869b9d95cce16c5a5e61c154aed41c39
Dillon Teakell <teakelld@icloud.com>
2025-10-15 14:24:20 -0400

    Clear Screen Button Feature Started

---
+++ Tip Calculator/Classes/Clear Screen Button/ClearScreenButton.h
@@ -0,0 +1,16 @@
+//
+//  ClearScreenButton.h
+//  Tip
+//
+//  Created by Dillon Teakell on 10/13/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClearScreenButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tip Calculator/Classes/Clear Screen Button/ClearScreenButton.m
@@ -0,0 +1,20 @@
+//
+//  ClearScreenButton.m
+//  Tip
+//
+//  Created by Dillon Teakell on 10/13/25.
+//
+
+#import "ClearScreenButton.h"
+
+@implementation ClearScreenButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
